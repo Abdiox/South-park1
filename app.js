db047e78b0da72ff623daf2a21d74db131b7e5c9
Abdiox <abdirahmanroble2610@gmail.com>
2023-03-23 00:20:23 +0100

    Added grid og gjort så man kan klikke på det

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 "use strict";
 
-window.addEventListener("load", initApp);
-
-function initApp() {
-  console.log("South Park is running!");
-}
+window.addEventListener("load", start);
 
 const tolkienBlack = {
   name: "Tolkien Black",
@@ -23,6 +19,68 @@ const tolkienBlack = {
   firstAppereance: "Cartman Gets an Anal Probe ",
 };
 
+function start() {
+  console.log("South Park is running!");
+  const data = getData();
+  //   showAllCharacters(data);
+
+  showCharacter(tolkienBlack);
+}
+
+function getData() {
+  const data = [];
+  return data;
+}
+
+function showCharacter(characters) {
+  const html = /*html*/ `<article class = "grid-item">
+  <img src=${characters.image}>
+  <p>${characters.name}</p>
+    <p>${characters.occupation}</p>
+  </article>
+`;
+  document.querySelector("#characters").insertAdjacentHTML("beforeend", html);
+  document.querySelector("#characters article:last-child").addEventListener("click", characterCliked);
+  function characterCliked() {
+    showDialog(characters);
+  }
+}
+
+function showDialog(characters) {
+  //Main
+  console.log(characters);
+  document.querySelector("#dialog_image").src = characters.image;
+  document.querySelector("#dialog_title").textContent = characters.name;
+  document.querySelector("#dialog_age").textContent = characters.age;
+  document.querySelector("#dialog_occupation").textContent = characters.occupation;
+
+  //Rest
+  document.querySelector("#dialog_gender").textContent = characters.gender;
+  document.querySelector("#dialog_religion").textContent = characters.religion;
+  document.querySelector("#dialog_catchPhrase").textContent = characters.catchPhrase;
+  document.querySelector("#dialog_hairColor").textContent = characters.hairColor;
+  document.querySelector("#dialog_schoolGrade").textContent = characters.schoolGrade;
+  document.querySelector("#dialog_episodes").textContent = characters.episodes;
+  document.querySelector("#dialog_appereances").textContent = characters.appereances;
+  document.querySelector("#dialog_firstAppereance").textContent = characters.firstAppereance;
+
+  document.querySelector("#dialog_name").textContent = characters.name;
+  document.querySelector("#dialog_voicedBy").textContent = characters.voicedBy;
+
+  // DIALOG ID:
+  document.querySelector("#dialog").showModal();
+
+  //   document.querySelector("#dialog").classList.remove("hidden");
+  //   document.querySelector("#btn_close").addEventListener("click", closeDialog);
+}
+
+// function closeDialog() {
+//   document.querySelector("#btn_close").addEventListener("click", closeDialog);
+//   document.querySelector("#dialog").classList.add("hidden");
+// }
+
+// SHOW CHARACTER FUNKTIONEN //
+
 // function showCharacter(character) {
 //   document.querySelector("#name").textContent = character.name;
 //   document.querySelector("#nickname").textContent = character.nickname;
@@ -38,28 +96,38 @@ const tolkienBlack = {
 //   document.querySelector("#appereances").textContent = character.appereances;
 //   document.querySelector("#firstAppereance").textContent = character.firstAppereance;
 // }
-const list = document.querySelector("#characters");
 
-function addCharacter(characters) {
-  const html = /*html*/ `
-     <li>
-     <h2> ${characters.name}  </h2> 
-      <p><strong>Nickname:</strong> ${characters.nickname}</p>
-      <img src="${characters.image}" alt="" style="width: 10%"   image">
-            <p><strong>Occupation:</strong> ${characters.occupation}</p>
-    <p><strong>Age:</strong> ${characters.age}</p>
-      <p><strong>Voiced By:</strong> ${characters.voicedBy}</p>
-      <p><strong>Gender:</strong> ${characters.gender}</p>
-<p><strong>Religion:</strong> ${characters.religion}</p>
-      <p><strong>Catch Phrase:</strong> ${characters.catchPhrase}</p>
-      <p><strong>Hair Color:</strong> ${characters.hairColor}</p>
-      <p><strong>School Grade:</strong> ${characters.schoolGrade}</p>
-      <p><strong>Episodes:</strong> ${characters.episodes}</p>
-            <p><strong>Appereances:</strong> ${characters.appereances}</p>
-      <p><strong>First Appereance:</strong> ${characters.firstAppereance}</p>
+// ADD CHARACTER FUNKTIONEN //
 
+// const list = document.querySelector("#characters");
 
-    </li>`;
-  list.insertAdjacentHTML("beforeend", html);
-}
-addCharacter(tolkienBlack);
+// function addCharacter(characters) {
+//   const html = /*html*/ `
+//      <li>
+//            <p><strong>Name:</strong> ${characters.name}</p>
+//       <p><strong>Nickname:</strong> ${characters.nickname}</p>
+//       <img src="${characters.image}" alt="" style="width: 6%"   image">
+//             <p><strong>Occupation:</strong> ${characters.occupation}</p>
+//     <p><strong>Age:</strong> ${characters.age}</p>
+//       <p><strong>Voiced By:</strong> ${characters.voicedBy}</p>
+//       <p><strong>Gender:</strong> ${characters.gender}</p>
+// <p><strong>Religion:</strong> ${characters.religion}</p>
+//       <p><strong>Catch Phrase:</strong> ${characters.catchPhrase}</p>
+//       <p><strong>Hair Color:</strong> ${characters.hairColor}</p>
+//       <p><strong>School Grade:</strong> ${characters.schoolGrade}</p>
+//       <p><strong>Episodes:</strong> ${characters.episodes}</p>
+//             <p><strong>Appereances:</strong> ${characters.appereances}</p>
+//       <p><strong>First Appereance:</strong> ${characters.firstAppereance}</p>
+
+//     </li>`;
+//   list.insertAdjacentHTML("beforeend", html);
+// }
+// addCharacter(tolkienBlack);
+
+// EN MÅDE AT SÆTTE FUNKTIONERNE PÅ //
+
+// function showAllCharacters(list) {
+//     showCharacter(character);
+// }
+
+// function getDescription() {}
