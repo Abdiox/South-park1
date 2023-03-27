4a67bfc6a7b59b54e31a4638db5b7c95593b38b4
Abdiox <abdirahmanroble2610@gmail.com>
2023-03-27 15:47:38 +0200

    Fetch array med alle JSON-objekter + lavet grid over det

--- app.js
+++ app.js
@@ -5,79 +5,15 @@ window.addEventListener("load", start);
 async function start() {
   console.log("South Park is running!");
 
-  const tolkienBlack = await getCharacter("https://raw.githubusercontent.com/Abdiox/South-park1/main/Data/southPark.json");
-  showCharacter(tolkienBlack);
+  const characters = await getCharacter();
 
-  const randy = await getCharacter("https://raw.githubusercontent.com/MadsFolkmann/Data-app-kode/main/data/randy.json");
-  showCharacter(randy);
-
-  const leopold = await getCharacter("https://raw.githubusercontent.com/Asbjoernemil/data-assignment/main/data/characters.json");
-  showCharacter(leopold);
-
-  const eric = await getCharacter("https://raw.githubusercontent.com/Bindholt/Data-Projekt/main/data/cartman.json");
-  showCharacter(eric);
-
-  const fosse = await getCharacter("https://raw.githubusercontent.com/frva0001/data/main/data/fosseMcDonald.json");
-  showCharacter(fosse);
-
-  const jimmy = await getCharacter("https://raw.githubusercontent.com/Forkeh/South-Park-App/main/data/jimmy.json");
-  showCharacter(jimmy);
-
-  const barack = await getCharacter("https://raw.githubusercontent.com/AliHMohammad/Data-Ali/main/obamaChar.json");
-  showCharacter(barack);
-
-  const donald = await getCharacter("https://raw.githubusercontent.com/BurimJ/Southpark-Character/main/trump.json");
-  showCharacter(donald);
-
-  const tuong = await getCharacter("https://raw.githubusercontent.com/TheDanishMexican/object-south-park/main/data/app.json");
-  showCharacter(tuong);
-
-  const kyle = await getCharacter("https://raw.githubusercontent.com/fili0727/Data-opgave/main/kyle.json");
-  showCharacter(kyle);
-
-  const towelie = await getCharacter("https://raw.githubusercontent.com/emsc0001/Towelie/main/Data/towelie.json");
-  showCharacter(towelie);
-
-  const paris = await getCharacter("https://raw.githubusercontent.com/Tunafan/southpark_db/main/South%20Park%20db/data/paris.json");
-  showCharacter(paris);
-
-  const jack = await getCharacter("https://raw.githubusercontent.com/YawHB/South_Park_Project/main/data/jack.json");
-  showCharacter(jack);
-
-  const jimbo = await getCharacter("https://raw.githubusercontent.com/Olivermadsenn/Data-afleverin-g/main/data/jimbo.json");
-  showCharacter(jimbo);
-
-  const scott = await getCharacter("https://raw.githubusercontent.com/luma0001/Data-V1-project/07f6ce395a9b87079df42f68b35483f405eb0c21/scott.json");
-  showCharacter(scott);
-
-  const abraham = await getCharacter("https://raw.githubusercontent.com/Loxiuz/Data_Aflevering/main/data/lincoln.json");
-  showCharacter(abraham);
-
-  const mrMackey = await getCharacter("https://raw.githubusercontent.com/MaxusTheOne/data_project/main/data/mrMackey.json");
-  showCharacter(mrMackey);
-
-  const kenny = await getCharacter("https://raw.githubusercontent.com/Mart0808DK/Data-fetch-projekt/main/kenny.json");
-  showCharacter(kenny);
-
-  const wendy = await getCharacter("https://raw.githubusercontent.com/osman-butt/data-app/main/wendy.json");
-  showCharacter(wendy);
-
-  const kennedy = await getCharacter("https://raw.githubusercontent.com/rarogbennu/SP/master/data/jfk.json");
-  showCharacter(kennedy);
-
-  const tomCruise = await getCharacter("https://raw.githubusercontent.com/svdf18/SPobjects/main/data/tomcruise.json");
-  showCharacter(tomCruise);
-
-  const peter = await getCharacter("https://raw.githubusercontent.com/Kapalee/South-park-project/main/data/pcPrincipal.json");
-  showCharacter(peter);
-
-  const stan = await getCharacter("https://raw.githubusercontent.com/VARattleff/south-park-viktor/main/sp.json");
-  showCharacter(stan);
+  characters.forEach(showCharacter);
 }
 
-async function getCharacter(url) {
-  const response = await fetch(url);
+async function getCharacter() {
+  const response = await fetch("southPark.json");
   const data = await response.json();
+  console.log(data);
   return data;
 }
 
@@ -114,6 +50,7 @@ function showDialog(characters) {
   document.querySelector("#dialog_appearances").textContent = characters.appearances;
   document.querySelector("#dialog_firstAppearance").textContent = characters.firstAppearance;
 
+  //Bottom
   document.querySelector("#dialog_name").textContent = characters.name;
   document.querySelector("#dialog_voicedBy").textContent = characters.voicedBy;
 
